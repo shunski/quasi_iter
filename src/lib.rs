b08f6a5b6a69c3ff8c6bdd635903a42370c02bb1
Shunsaku Yadokoro <y.thyks@gmail.com>
2024-03-11 16:54:44 -0400

    trusted_len removed from unchecked iterator

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,16 @@ pub mod merge;
 pub mod dedup;
 // pub mod splice;
 
-/// The trait `UncheckedIterator` is just `Iterator` but the return value is forced to be 'Item'. This means that
-/// the iterator must be stopped before the iterator run.
-/// The trait itself is unsafe, because the safety of `next_unchecked` depends on `trusted_len`.
-/// Note that this trait may be deprecated after the trait `std::iter::TrustedLen` becomes stable.
+/// The trait `UncheckedIterator` is just `Iterator` but the return value is forced to be 'Self::Item' instead of `Option<Self::Item>`. 
+/// This means that the iterator must be stopped before it runs out of its elements.
+/// The trait itself is NOT unsafe, because the unsafe method `next_unchecked` is the only method.
+/// Note that this trait is different from `UncheckedIterator` in the standard library (privated trait in `core`) in the sense that `UncheckedIterator` in this crate
+/// is supposed by an external condition. Hence, it does not require the struct to have the length information, i.e. the trusted `size_hint` is not required.
 /// # Safety:
 /// `next_unchecked` must be safe to call `self.trusted_len()` times.
-pub unsafe trait UncheckedIterator {
+pub trait UncheckedIterator {
     type Item;
     unsafe fn next_unchecked(&mut self) -> Self::Item;
-    fn trusted_len(&self) -> usize;
 }
 
 pub mod prelude {
